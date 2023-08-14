1b33e788427fff56b653323b04561103e4539c37
Alexander <41678109+WhoIsDarth@users.noreply.github.com>
2023-08-14 22:57:41 +0200

    Pushed

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import uuid
 from decimal import Decimal
+import socket
 
 from app.common.config import settings
 from app.db.database import ClickHousePool
@@ -22,7 +23,22 @@ logging.basicConfig(
 )
 
 
+async def tcp_health_check(port: int):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('0.0.0.0', port))
+        s.listen()
+        logging.info(f"Health check listening on port {port}")
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                logging.info(f"Connected by {addr}")
+                conn.close()
+
+
 async def main():
+    # Start the health check server
+    asyncio.create_task(tcp_health_check(8080))
+
     logging.info("Starting data collection")
     logging.info(f"Launch ID: {launch_id}")
 
