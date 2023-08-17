bb2447c76c2d59c03a05d3b4ce6194c98f11b147
Alexander <41678109+WhoIsDarth@users.noreply.github.com>
2023-08-17 19:23:18 +0200

    fixed clickhouse clients

--- app/db/database.py
+++ app/db/database.py
@@ -1,24 +1,23 @@
 import asyncio
-
 from clickhouse_driver import Client
-
 from app.common.config import settings
 
 
 class ClickHousePool:
     def __init__(self, max_connections=5):
         self._semaphore = asyncio.Semaphore(max_connections)
+        self._clients = []
         self._host = settings.CLICKHOUSE_HOST
         self._port = settings.CLICKHOUSE_PORT
         self._username = settings.CLICKHOUSE_USERNAME
         self._password = settings.CLICKHOUSE_PASSWORD
         self._database = settings.CLICKHOUSE_DATABASE
 
-    async def execute(self, query, params=None):
-        async with self._semaphore:
-            return await asyncio.to_thread(self._execute_sync, query, params)
+        for _ in range(max_connections):
+            client = self._create_client()
+            self._clients.append(client)
 
-    def _execute_sync(self, query, params):
+    def _create_client(self):
         connection_settings = {
             "host": self._host,
             "port": self._port,
@@ -30,5 +29,15 @@ class ClickHousePool:
             connection_settings["secure"] = True
             connection_settings["verify"] = False
 
-        with Client(**connection_settings) as client:
-            return client.execute(query, params)
+        return Client(**connection_settings)
+
+    async def execute(self, query, params=None):
+        async with self._semaphore:
+            client = self._clients.pop()
+            try:
+                return await asyncio.to_thread(self._execute_sync, client, query, params)
+            finally:
+                self._clients.append(client)
+
+    def _execute_sync(self, client, query, params):
+        return client.execute(query, params)
