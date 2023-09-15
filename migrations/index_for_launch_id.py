efae2c2883e8bd30354003c2cb033978bb727703
Alexander <41678109+WhoIsDarth@users.noreply.github.com>
2023-09-15 19:21:20 +0200

    migration

---
+++ migrations/index_for_launch_id.py
@@ -0,0 +1,32 @@
+import os
+
+from clickhouse_driver import Client
+
+
+def create_table(client):
+    client.execute(
+        """
+    ALTER TABLE binance_orderbook ADD INDEX launch_id_index (launch_id) TYPE minmax GRANULARITY 1;
+    SET allow_create_index_without_type=1;
+    """
+    )
+
+
+def main():
+    connection_settings = {
+        "host": os.getenv("CLICKHOUSE_HOST"),
+        "port": int(os.getenv("CLICKHOUSE_PORT")),
+        "user": os.getenv("CLICKHOUSE_USERNAME"),
+        "password": os.getenv("CLICKHOUSE_PASSWORD"),
+        "database": os.getenv("CLICKHOUSE_DATABASE"),
+    }
+    if int(os.getenv("CLICKHOUSE_PORT")) == 9440:
+        connection_settings["secure"] = True
+        connection_settings["verify"] = False
+
+    client = Client(**connection_settings)
+    create_table(client)
+
+
+if __name__ == "__main__":
+    main()
