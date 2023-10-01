52510b0db02a4a24d4e25c8c4505f46cea99ffb1
Alexander <41678109+WhoIsDarth@users.noreply.github.com>
2023-10-01 21:56:06 +0200

    fixed celery worker

--- app/common/worker.py
+++ app/common/worker.py
@@ -6,6 +6,6 @@ from app.workers.order_book_worker import order_book_table_truncate_and_backup
 worker.conf.beat_schedule = {
     "order-book-table-truncate-and-backup-daily-at-23-utc": {
         "task": order_book_table_truncate_and_backup.name,
-        "schedule": crontab(hour=23, minute=0),
+        "schedule": crontab(hour=22, minute=30),
     },
 }
