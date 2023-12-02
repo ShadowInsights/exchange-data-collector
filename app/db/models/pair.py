8a742863f44b265c59420dfa3b3def708d453129
Matster07 <60222325+Matster07@users.noreply.github.com>
2023-12-03 01:31:23 +0300

    Remove migration. Returned old model version (#52)

--- app/db/models/pair.py
+++ app/db/models/pair.py
@@ -11,7 +11,9 @@ from app.common.database import BaseModel
 class PairModel(BaseModel):
     __tablename__ = "pairs"
 
-    symbol: Mapped[str] = mapped_column(String, nullable=False, index=True)
+    symbol: Mapped[str] = mapped_column(
+        String, nullable=False, index=True, unique=True
+    )
     delimiter: Mapped[Decimal] = mapped_column(DECIMAL, nullable=False)
     exchange_id: Mapped[UUID] = mapped_column(
         pg_UUID(as_uuid=True), ForeignKey("exchanges.id"), nullable=False
